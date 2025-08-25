cf8852d3d1a36faf02b2e2010e55e7eb6b14d423
lfourage <ludogriph@gmail.com>
2025-08-25 15:20:01 +0200

    corrected PrismaErrorHandle

--- lib/utils/handlePrismaError.ts
+++ lib/utils/handlePrismaError.ts
@@ -4,20 +4,25 @@ import {
 } from "@prisma/client/runtime/library";
 
 export const handlePrismaError = (error: unknown) => {
-  if (error instanceof PrismaClientKnownRequestError) {
-    switch (error.code) {
-      case "P2002": {
-        return { status: 409, body: { error: error.message } };
-      }
-      case "P2025": {
-        return { status: 404, body: { error: error.message } };
-      }
-      default: {
-        return { status: 400, body: { error: error.message } };
-      }
+  if (
+    error instanceof PrismaClientKnownRequestError ||
+    (typeof error === "object" &&
+      error !== null &&
+      "code" in error &&
+      typeof (error as any).code === "string")
+  ) {
+    const e = error as PrismaClientKnownRequestError;
+    switch (e.code) {
+      case "P2002":
+        return { status: 409, body: { error: e.message } };
+      case "P2025":
+        return { status: 404, body: { error: e.message } };
+      default:
+        return { status: 400, body: { error: e.message } };
     }
   } else if (error instanceof PrismaClientValidationError) {
     return { status: 422, body: { error: error.message } };
   }
+
   return null;
 };

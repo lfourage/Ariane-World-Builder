8a56f82914e213ba78a7748b6cd3486d84d626bd
lfourage <ludogriph@gmail.com>
2025-08-27 16:37:18 +0200

    updated schema for expected crud

--- lib/schemas/eventSchema.ts
+++ lib/schemas/eventSchema.ts
@@ -1,18 +1,24 @@
 import { z } from "zod";
 
 const EventNodeDataSchema = z.object({
-  description: z.string(),
+  description: z.string().optional(),
+  date: z.string().optional(),
+  importance: z.number().optional(),
+  tags: z.array(z.string()).optional(),
 });
-
+/*
 const EventConnectionSchema = z.object({
   type: z.enum(["LINEAR", "TIMETRAVEL"]),
   nextId: z.cuid().optional(),
   prevId: z.cuid().optional(),
-});
+});*/
 
 export const CreateEventSchema = z.object({
   title: z.string(),
   data: EventNodeDataSchema,
-  nexts: z.array(EventConnectionSchema).optional(),
-  prevs: z.array(EventConnectionSchema).optional(),
+});
+
+export const UpdateEventSchema = z.object({
+  title: z.string().optional(),
+  data: EventNodeDataSchema.optional(),
 });
